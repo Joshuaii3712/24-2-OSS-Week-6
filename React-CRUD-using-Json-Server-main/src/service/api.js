dd76f3c4583760aebaee84cc74ef173523aa28be
Joshuaii3712 <joshuaii3712@gmail.com>
2024-10-06 21:52:58 +0900

    Update package.json and api.js to connect to mockapi.io service

--- React-CRUD-using-Json-Server-main/src/service/api.js
+++ React-CRUD-using-Json-Server-main/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const url = "http://127.0.0.1:3003/user";
+const url = "https://66ff4a442b9aac9c997ed41e.mockapi.io/api/userData";
 
 export const getallUsers = async (id) => {
     id = id || '';
